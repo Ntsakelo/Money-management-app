aafb1169203f1e97fea641f07a880f9b85b2fe79
Ntsakelo <Ntsakeloarnold00@gmail.com>
2022-06-08 18:29:23 +0200

    changes

--- money_management_dom.js
+++ money_management_dom.js
@@ -1,14 +1,39 @@
 // referencing the details form
-const firstName = document.querySelector('.firstName');
-const lastName = document.querySelector('.lastName');
-const salary = document.querySelector('.salary');
-const expenses = document.querySelector('.expenses');
-const getAdviceBtn = document.querySelector('.getAdvice');
+const firstName = document.querySelector(".firstName");
+const lastName = document.querySelector(".lastName");
+const salary = document.querySelector(".salary");
+const expenses = document.querySelector(".expenses");
+const advice = document.querySelector(".nextPage");
+//reference to the finAdvice page
+
+const electAmt = document.querySelector(".electAmt");
+
+const groceryAmt = document.querySelector(".groceryAmt");
+
+const otherAmt = document.querySelector(".otherAmt");
+
+const debtAmt = document.querySelector(".debtAmt");
+
+const saveAmt = document.querySelector(".saveAmt");
+
+//Button to see the graph
+const getGraph = document.querySelector(".getGraph");
+const getAdviceBtn = document.querySelector(".getAdvice");
 
 // instance of factory function
-const moneyApp = "";
+const moneyApp = FinAdvice();
 
 //
-getAdviceBtn.addEventListener('click', function(){
-    
+getAdviceBtn.addEventListener("click", function () {
+  moneyApp.setUserName(firstName.value);
+  moneyApp.setlstName(lastName.value);
+  moneyApp.setSalary(Number(salary.value));
+  moneyApp.setExpense(Number(expenses.value));
+
+  electAmt.innerHTML = moneyApp.calcElectricity();
+  groceryAmt.innerHTML = moneyApp.calcGrocery();
+  otherAmt.innerHTML = moneyApp.calcOtherExp();
+  debtAmt.innerHTML = moneyApp.calcDebts();
+  saveAmt.innerHTML = moneyApp.calcSavings();
+  advice.setAttribute("style", "transform:translateX(0px);");
 });
