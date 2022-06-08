bd7c0d2054339e964102cf6013fd8f9cd52fca74
WalterBM68 <Wbmanjanja@gmail.com>
2022-06-08 14:10:36 +0200

    dom structure

---
+++ money_management_dom.js
@@ -0,0 +1,14 @@
+// referencing the details form
+const firstName = document.querySelector('.firstName');
+const lastName = document.querySelector('.lastName');
+const salary = document.querySelector('.salary');
+const expenses = document.querySelector('.expenses');
+const getAdviceBtn = document.querySelector('.getAdvice');
+
+// instance of factory function
+const moneyApp = "";
+
+//
+getAdviceBtn.addEventListener('click', function(){
+    
+});
